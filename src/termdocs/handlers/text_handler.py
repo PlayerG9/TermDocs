2a7d0d2c904817d8c3c388053904b4534c9f578d
PlayerG9 <g9yout@gmail.com>
2023-08-15 17:25:23 +0200

    configured syntax highlighting

--- src/termdocs/handlers/text_handler.py
+++ src/termdocs/handlers/text_handler.py
@@ -5,6 +5,7 @@ r"""
 """
 from pathlib import Path
 import textual.app
+import textual.widgets
 import rich.syntax
 from .basehandler import BaseHandler
 from .register import register_handler
@@ -18,10 +19,25 @@ class TextHandler(BaseHandler):
         mime, _ = mimetypes.guess_type(file.name)
         return mime and mime.startswith("text/")
 
-    def render(self) -> textual.app.RenderableType:
-        try:
-            return rich.syntax.Syntax.from_path(
-                path=str(self.filepath),
-            )
-        except Exception:
-            return self.filepath.read_text()
+    def compose(self) -> textual.app.ComposeResult:
+        yield textual.widgets.Static(markup=False)
+
+    def on_mount(self):
+        text = self.query_one(textual.widgets.Static)
+        content = self.filepath.read_text()
+        lexer = rich.syntax.Syntax.guess_lexer(path=str(self.filepath))
+        syntax = rich.syntax.Syntax(
+            code=content,
+            lexer=lexer,
+            theme='ansi_dark',
+            dedent=False,
+            line_numbers=False,
+            # highlight_lines={2,3,4,5,6,7,8},
+            # code_width=80,
+            tab_size=4,
+            word_wrap=False,
+            background_color=None,
+            indent_guides=False,
+            padding=0,
+        )
+        text.update(syntax)
